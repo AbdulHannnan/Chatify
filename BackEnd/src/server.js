1a3cfbe6729c2056574fc1367c5acc9c8c034dfe
Abdul Hannan <ahk12067@gmail.com>
2026-06-06 17:35:56 +0500

    Dynamic Routing codeBase is developed

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -4,28 +4,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import authRoutes from '../routes/auth.routes.js';
+
 const PORT= process.env.PORT;
 
 const app = express();
 
 
-app.get('/api/auth/signup', (req, res) => {
-    res.send('Signup route');
-});
-
-app.get('/api/auth/login', (req, res) => {
-    res.send('Login route');
-});
-
-app.get('/api/auth/logout', (req, res) => {
-    res.send('Logout route');
-});
+app.use("/api/auth", authRoutes);
 
 // Ensure you have something like this:
 app.get('/', (req, res) => {
   res.send('Hello! The server is working.');
 });
 
-app.listen(process.env.PORT || 3001, () => {
-    console.log('Server is running on port', process.env.PORT || 3001);
+app.listen(PORT || 3001, () => {
+    console.log('Server is running on port', PORT || 3001);
 });
