106f7e85f8f88a6ba41a46bf6cfd11038399e088
Abdul Hannan <ahk12067@gmail.com>
2026-06-07 17:06:31 +0500

    Single Localhost created for the both frontEnd and backEnd

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-
+import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,9 +11,20 @@ const PORT= process.env.PORT;
 
 const app = express();
 
+const __dirname = path.resolve();
+
 
 app.use("/api/auth", authRoutes);
 app.use('/api/message', messageRoutes);
+
+
+// Make ready for the Deployment
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../FrontEnd/dist')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../FrontEnd/dist/index.html'))
+    });
+  };
 
 // Ensure you have something like this:
 app.get('/', (req, res) => {
