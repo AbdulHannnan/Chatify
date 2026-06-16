185ff97144e963f798a2f1d16aa30590f2b2684d
Abdul Hannan <ahk12067@gmail.com>
2026-06-16 23:57:56 +0500

    some changes to utils

--- BackEnd/lib/utils.js
+++ BackEnd/lib/utils.js
@@ -1,7 +1,11 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId, res) => {
-  if (!process.env.JWT_SECRET) {
+
+  // After the CodeRabit Review
+
+  const { JWT_SECRET } = process.env;
+  if(!JWT_SECRET) {
     throw new Error('JWT_SECRET is missing in .env file');
   }
 
