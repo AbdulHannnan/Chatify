3f9dc06bc9b3a23197c796cdc658f94b57c68e42
Abdul Hannan <ahk12067@gmail.com>
2026-06-16 23:55:00 +0500

    some security enhancement to DB

--- BackEnd/lib/db.js
+++ BackEnd/lib/db.js
@@ -2,6 +2,13 @@ import mongoose from 'mongoose';
 
 const connectDB = async ()=>{
     try{
+
+        // After the CodeRabit Review 
+        const {MONGO_URI} = process.env;
+        if(!MONGO_URI){
+            throw new Error('MONGO_URI is missing in .env file');
+        }
+
         mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected successfully:', mongoose.connection.host);
     }
