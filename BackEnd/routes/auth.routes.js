ba1406210ef263ffd0fc14c9ed59eaaa9e1d2152
Abdul Hannan <ahk12067@gmail.com>
2026-06-24 16:19:23 +0500

    Begining the Update Route

--- BackEnd/routes/auth.routes.js
+++ BackEnd/routes/auth.routes.js
@@ -1,14 +1,13 @@
 import express from 'express';
-import { signup , login , logout } from '../controller/auth.controler.js';
+import { signup , login , logout, updateProfile } from '../controller/auth.controler.js';
 
 const router = express.Router();
 
 
 router.post('/signup', signup)
-
-
 router.post('/login', login);
-
 router.get('/logout', logout);
+
+router.put('/update-profile', protectRoute, updateProfile);
 
 export default router;
