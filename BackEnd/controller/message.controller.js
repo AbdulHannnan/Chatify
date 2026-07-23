33ba99d38fe086cdacd55f9722110efdd937d83d
Abdul Hannan <ahk12067@gmail.com>
2026-07-23 17:44:59 +0500

    Single Person chat Controller is done

--- BackEnd/controller/message.controller.js
+++ BackEnd/controller/message.controller.js
@@ -14,3 +14,25 @@ export const getAllContacts = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+
+export const getMessagesByUserId = async (req, res) => {
+    try{
+        const myId = req.user._id;
+        const {id: userToChatId} = req.params;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId }
+            ]
+        }).sort({ createdAt: 1 });
+
+        res.status(200).json(messages);
+
+    } catch (error) {
+        console.log("Error in getMessagesByUserId:", error);
+        res.status(500).json({ message: "Server error" });   
+
+    }
+}
