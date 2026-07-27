2765ab43c9f786f8613f1ebe31be1a67f1c402ec
Abdul Hannan <ahk12067@gmail.com>
2026-07-27 20:15:10 +0500

    Frontend setup done

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+
+import { Routes , Route } from 'react-router-dom'
+import chatPage from './pages/chatPage'
+import loginPage from './pages/loginPage'
+import signUpPage from './pages/signUpPage'
+
+
 
 function App() {
   return (
-    <div>
-         <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
 
-    <button className="btn btn-primary">Click me</button>
-    </div>
+     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      {/* DECORATORS - GRID BG & GLOW SHAPES */}
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+
+    
+          <Routes>
+            <Route path="/" element={<chatPage />} />
+            <Route path="/login" element={<loginPage />} />
+            <Route path="/signup" element={<signUpPage />} />
+          </Routes>
+          
+  </div>
   )
 }
 
